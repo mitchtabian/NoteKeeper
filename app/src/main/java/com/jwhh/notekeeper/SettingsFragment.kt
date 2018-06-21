d892fce6688dff10dd5f4df3f4158e9e372fdef1
Mitch <mitch@tabian.ca>
2018-06-21 13:05:10 -0700

    3.3 end

--- app/src/main/java/com/jwhh/notekeeper/SettingsFragment.kt
+++ app/src/main/java/com/jwhh/notekeeper/SettingsFragment.kt
@@ -1,6 +1,29 @@
 package com.jwhh.notekeeper
 
+import android.os.Bundle
+import android.preference.Preference
 import android.preference.PreferenceFragment
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 
 class SettingsFragment: PreferenceFragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.pref_main)
+
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        val view: View = super.onCreateView(inflater, container, savedInstanceState)
+        view.setBackgroundColor(ContextCompat.getColor(activity, R.color.white))
+
+        return view
+    }
 }
