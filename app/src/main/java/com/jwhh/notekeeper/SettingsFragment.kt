0ad4837369c3a69e9c6a69fb03ae34eee8377eb8
Mitch <mitch@tabian.ca>
2018-06-21 14:44:57 -0700

    3.4 start

--- app/src/main/java/com/jwhh/notekeeper/SettingsFragment.kt
+++ app/src/main/java/com/jwhh/notekeeper/SettingsFragment.kt
@@ -3,6 +3,8 @@ package com.jwhh.notekeeper
 import android.os.Bundle
 import android.preference.PreferenceFragment
 import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -20,6 +22,13 @@ class SettingsFragment: PreferenceFragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val view: View = super.onCreateView(inflater, container, savedInstanceState)
         view.setBackgroundColor(ContextCompat.getColor(activity, R.color.white))
+
+        val activity = activity as AppCompatActivity
+        activity.setSupportActionBar(activity.findViewById(R.id.settings_toolbar) as Toolbar)
+        val actionBar = activity.supportActionBar!!
+        actionBar.setDisplayHomeAsUpEnabled(true)
+        actionBar.setTitle(R.string.fragment_settings)
+        setHasOptionsMenu(true)
 
         return view
     }
