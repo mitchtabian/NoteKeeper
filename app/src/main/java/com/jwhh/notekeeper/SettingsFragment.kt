163b01306168364d4304a7dd3e9b6127a94a2ea6
Mitch <mitch@tabian.ca>
2018-06-26 13:44:06 -0700

    3.4 start

--- app/src/main/java/com/jwhh/notekeeper/SettingsFragment.kt
+++ app/src/main/java/com/jwhh/notekeeper/SettingsFragment.kt
@@ -6,6 +6,7 @@ import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.LayoutInflater
+import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 
